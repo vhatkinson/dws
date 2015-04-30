d68d20c275537e26e53098d5b1e290dd834273c9
vhatkinson <vernard12345@GMail.com>
2015-04-30 17:46:42 -0400

    deleted js

--- assignment4/js/untitled text 2.js
+++
@@ -1,7 +0,0 @@
-while(min===""||isNaN(min)){
-    if(min===""){
-        min=prompt("what's the minimum value? \nPlease do not leave blank.");
-    }else{
-        min=prompt("what's the minimum value? \nPlease only type in numbers.");
-    }
-}
