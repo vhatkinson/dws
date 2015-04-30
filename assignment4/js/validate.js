138648d9b9f3f4693a5ce37f9b091edee5318afe
vhatkinson <vernard12345@GMail.com>
2015-04-30 17:08:45 -0400

    assignment 4

---
+++ assignment4/js/validate.js
@@ -0,0 +1,7 @@
+while(min===""||isNaN(min)){
+    if(min===""){
+        min=prompt("what's the minimum value? \nPlease do not leave blank.");
+    }else{
+        min=prompt("what's the minimum value? \nPlease only type in numbers.");
+    }
+}
